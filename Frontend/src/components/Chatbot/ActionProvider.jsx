591f01986d731a38864f120ee4007c9f462ac8c8
saritagud <gudinop864@gmail.com>
2023-04-10 14:55:15 -0400

    en proceso la conexion al backend del chat

--- Frontend/src/components/Chatbot/ActionProvider.jsx
+++ Frontend/src/components/Chatbot/ActionProvider.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const handleHello=(message) => {
+    
+    const botMessage = createChatBotMessage(message)
+
+    setState((prev)=>({
+      ...prev,
+      messages:[...prev.messages,botMessage],
+    }));
+  };
+  
+  
+  
   return (
     <div>
       {React.Children.map(children, (child) => {
         return React.cloneElement(child, {
-          actions: {},
+          actions: {
+            handleHello,
+          },
         });
       })}
     </div>
