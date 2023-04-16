48906c57608799d64719068b308a1b8c7146fb01
Atilio García <atiliojg1@gmail.com>
2023-04-16 19:12:56 -0400

    En register.jsx se añadió el token a localStorage para dejar la sesión iniciada luego del registro, en vista de esto, ahora se redirige a la vista de personalización una vez hecho el registro exitoso. En login.jsx se añadió una pequeña validación, de manera que si el token es null o undefined, entonces no se guarde en localStorage. En la vista de personalización.jsx se realizó la lógica para registrar valores del 1 al 3 en los menú de selección de tema y sonido, luego con fetch se manda al backend para actualizar las preferencias del usuario en la BD.

--- Frontend/src/components/personalizacion.jsx
+++ Frontend/src/components/personalizacion.jsx
@@ -1,35 +1,64 @@
-import { useEffect } from 'react'
-import Footer from './footer'
+import { useState, useEffect } from "react";
+import Footer from "./footer";
 import { FaArrowLeft } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-function Personalizar (){
+function Personalizar() {
     const navigate = useNavigate();
-    const existeToken = localStorage.getItem('token');
+    const existeToken = localStorage.getItem("token");
     const estiloLabel = "font-Urbanist text-xl text-left w-full mt-8 font-bold";
     const estiloSelect = "h-14 rounded-xl p-3 font-Urbanist font-bold text-xl border-2 border-verdeOscuroFuerte";
+    const [temaSeleccionado, setTemaSeleccionado] = useState(1);
+    const [sonidoSeleccionado, setSonidoSeleccionado] = useState(1);
 
     useEffect(() => {
-        if ( !existeToken ) navigate('/');
-    }, [])
-    
+        if (!existeToken) navigate("/");
+    }, []);
 
-    return(
-        <div className='flex flex-col items-center'>
-            <i className='w-full text-left' onClick={() => navigate('/')}>
+    const handleSubmit = (event) => {
+        event.preventDefault();
+            console.log(`El tema seleccionado es: ${temaSeleccionado}`);
+            console.log(`El sonido seleccionado es: ${sonidoSeleccionado}`);
+
+            const datosUsuario = {
+                estilosPref: temaSeleccionado,
+                sonidoPref: sonidoSeleccionado,
+            }
+            
+            fetch("http://localhost:3000/personalizacion", {
+                method: "PUT",
+                body: JSON.stringify(datosUsuario),
+                headers: {
+                  "Content-Type": "application/json",
+                  "Authorization": `Bearer ${existeToken}`
+                }
+              })
+              .then(response => {
+                if (!response.ok) {
+                  throw new err("Error al actualizar los datos");
+                }
+                console.log("Datos actualizados correctamente");
+              })
+              .catch(err => {
+                console.error(err);
+              });
+            
+        
+    };
+    return (
+        <div className="flex flex-col items-center">
+            <i className="w-full text-left" onClick={() => navigate("/")}>
                 {" "}
                 <FaArrowLeft className="cursor-pointer text-4xl text-verdeOscuro ml-7 mt-7" />{" "}
             </i>
-            <h1 className='font-signikaNegative text-5xl text-center m-4'>¡Personaliza tu MomoyBOT!</h1>
+            <h1 className="font-signikaNegative text-5xl text-center m-4">¡Personaliza tu MomoyBOT!</h1>
 
-            <p className='font-Urbanist text-center text-[20px] m-5'>Al proveer esta información, MomoyBOT te podrá proporcionar mejores respuestas</p>
+            <p className="font-Urbanist text-center text-[20px] m-5">Al proveer esta información, MomoyBOT te podrá proporcionar mejores respuestas</p>
 
-            <form className="flex flex-col  bg-verdeClaro h-auto w-72 lg:w-80 rounded-2xl border-2 border-solid border-grisClaro p-4 mb-16 shadow-2xl">
-            
-            <div className="w-62 lg:w-80 rounded-t-2xl h-6 bg-verdeManzana -m-5 mb-1 border-t-2 border-grisClaro border-l-2 border-r-2"></div>
+            <form className="flex flex-col  bg-verdeClaro h-auto w-72 lg:w-80 rounded-2xl border-2 border-solid border-grisClaro p-4 mb-16 shadow-2xl" onSubmit={handleSubmit}>
+                <div className="w-62 lg:w-80 rounded-t-2xl h-6 bg-verdeManzana -m-5 mb-1 border-t-2 border-grisClaro border-l-2 border-r-2"></div>
                 <label className={estiloLabel}>Carrera</label>
-                <select className='h-14 rounded-xl text-base pl-3 font-Urbanist font-bold lg:text-xl border-2 border-verdeOscuroFuerte'>
-                
+                <select className="h-14 rounded-xl text-base pl-3 font-Urbanist font-bold lg:text-xl border-2 border-verdeOscuroFuerte">
                     <option>Ingeniería en Computación</option>
                     <option>Ingeniería Industrial</option>
                     <option>Administración de empresas</option>
@@ -37,21 +66,26 @@ function Personalizar (){
                     <option>Derecho</option>
                 </select>
 
-                <label className={estiloLabel}>Trimestre</label>
-                <input className='border-2 border-verdeOscuroFuerte h-14 text-left w-full rounded-2xl p-3 font-Urbanist text-xl font-bold' type="text"></input>
-
                 <label className={estiloLabel}>Tema</label>
-                <select className={estiloSelect}></select>
+                <select className={estiloSelect} value={temaSeleccionado} onChange={(e) => setTemaSeleccionado(e.target.value)}>
+                    <option value={1}>Tema 1</option>
+                    <option value={2}>Tema 2</option>
+                    <option value={3}>Tema 3</option>
+                </select>
 
                 <label className={estiloLabel}>Sonido de mensajes</label>
-                <select className={estiloSelect}></select>
+                <select className={estiloSelect} value={sonidoSeleccionado} onChange={(e) => setSonidoSeleccionado(e.target.value)}>
+                    <option value={1}>Sonido 1</option>
+                    <option value={2}>Sonido 2</option>
+                    <option value={3}>Sonido 3</option>
+                </select>
 
                 <button className="bg-verdeOscuroFuerte border-2  rounded-2xl font-Urbanist text-white text-xl p-3 w-52 font-bold m-10">Guardar Cambios</button>
             </form>
 
-            <Footer/>
+            <Footer />
         </div>
-    )
+    );
 }
 
 export default Personalizar;
